193ae57993416b966c89deccfdb0da39cb20cf49
DUONG Phu-Hiep <duongphuhiep@gmail.com>
2022-06-14 21:29:10 +0200

    upgrade depedencies and add fontawesome

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,34 +1,13 @@
 <script lang="ts">
-  let count: number = 0
+  let count: number = 0;
   const increment = () => {
-    count += 1
-  }
+    count += 1;
+  };
 </script>
 
-<button on:click={increment}>
+<button
+  on:click={increment}
+  class="px-5 py-3 text-white bg-red-500 border-solid shadow-md hover:bg-red-600 focus:ring-1 focus:outline-none focus:ring-opacity-30"
+>
   Clicks: {count}
 </button>
-
-<style>
-  button {
-    font-family: inherit;
-    font-size: inherit;
-    padding: 1em 2em;
-    color: #ff3e00;
-    background-color: rgba(255, 62, 0, 0.1);
-    border-radius: 2em;
-    border: 2px solid rgba(255, 62, 0, 0);
-    outline: none;
-    width: 200px;
-    font-variant-numeric: tabular-nums;
-    cursor: pointer;
-  }
-
-  button:focus {
-    border: 2px solid #ff3e00;
-  }
-
-  button:active {
-    background-color: rgba(255, 62, 0, 0.2);
-  }
-</style>
